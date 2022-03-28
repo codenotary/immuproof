8e10931cac153d9296e6a06701753853c858bdb0
alpsenelcn <alp@codenotary.com>
2022-03-28 13:27:21 +0300

    FEATURE: UI building

--- vue/src/helpers/helpers.js
+++ vue/src/helpers/helpers.js
@@ -4,8 +4,17 @@ export function formattedDateLocaleString(
 		weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'
 	}
 ) {
-	console.log(date)
 	const options = { ...extraOptions, weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
 	return new Date(date).toLocaleTimeString('en-US', options);
+}
+
+export function numFormatter(num) {
+	const suffixes = ["", "k", "m", "b", "t", ''];
+	const suffixNum = Math.floor((""+num).length/3);
+	let shortValue = parseFloat((suffixNum !== 0 ? (num / Math.pow(1000,suffixNum)) : num).toPrecision(2));
+	if (shortValue % 1 !== 0) {
+		shortValue = shortValue.toFixed(1);
+	}
+	return shortValue+suffixes[suffixNum];
 }
