c16f011c60a3827d386ce842e4d4ad4d86dc9777
Michele Meloni <cleaversdev@gmail.com>
2022-04-06 15:03:28 +0200

    test: increase test coverage

--- cnc/client_test.go
+++ cnc/client_test.go
@@ -39,3 +39,22 @@ func TestNewCNCClientTLS(t *testing.T) {
 	_, err = NewCNCClient(lcApiKey, host, port, lcCertPath, skipTlsVerify, noTls)
 	require.Error(t, err)
 }
+
+func TestNewCNCClientNOTLS(t *testing.T) {
+	lcApiKey := "test_api_key"
+	host := "test_host"
+	port := "8080"
+	lcCertPath := "./cnctest/test_server.crt"
+
+	c, err := NewCNCClient(lcApiKey, host, port, lcCertPath, true, true)
+	require.Contains(t, err.Error(), "illegal parameters submitted: skip-tls-verify and no-tls arguments are both provided")
+
+	c, err = NewCNCClient(lcApiKey, host, "wrong", lcCertPath, false, false)
+	require.Contains(t, err.Error(), "ledger compliance port is invalid")
+
+	c, err = NewCNCClient(lcApiKey, host, port, lcCertPath, false, true)
+	require.NoError(t, err)
+	if c == nil {
+		t.Error("NewCNCClient() returned nil")
+	}
+}
