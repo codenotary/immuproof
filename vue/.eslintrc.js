7d23c576e0f0531ce29d8727aaed9da3cbd13894
Rafael Veggi <rafaelveggi@gmail.com>
2022-04-07 10:00:51 +0100

    fix no-console

--- vue/.eslintrc.js
+++ vue/.eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
